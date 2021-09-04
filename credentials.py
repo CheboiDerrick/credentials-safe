5d39fd232d79cd53abf04857b2b237091967e067
CheboiDerrick <derrick.cheboi@student.moringaschool.com>
2021-09-04 17:33:06 +0300

    added credentials class and the constructor for the class

---
+++ credentials.py
@@ -0,0 +1,8 @@
+class Credential:
+    credentials_list=[]
+    def __init__(self,sitename,username,password):
+        self.sitename=sitename
+        self.username=username
+        self.password=password
+
+    
